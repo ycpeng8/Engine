61d70fa5b92b42a11dbd60e9c49a886ee0a98be5
ycpeng-win <ycpeng@bu.edu>
2024-06-07 03:05:10 -0400

    init

---
+++ RenderManager/RenderManager.cpp
@@ -0,0 +1,6 @@
+//
+// Created by black on 6/5/2024.
+//
+
+#include "RenderManager.h"
+
---
+++ RenderManager/RenderManager.h
@@ -0,0 +1,17 @@
+//
+// Created by black on 6/5/2024.
+//
+
+#ifndef ENGINE_RENDERMANAGER_H
+#define ENGINE_RENDERMANAGER_H
+
+
+class RenderManager
+{
+public:
+    RenderManager() {}
+    ~RenderManager() {}
+};
+
+
+#endif //ENGINE_RENDERMANAGER_H
